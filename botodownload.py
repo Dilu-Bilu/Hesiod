0b00f81cf20e46bd2f7fed53d141f58dc205a1d4
Dilu-Bilu <dilreetraju@gmail.com>
2023-02-12 19:01:43 -0800

    removed the botodownlaod

--- botodownload.py
+++ botodownload.py
@@ -1,16 +1,16 @@
-import os
-import boto3
-from config.settings.production import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME 
-# Define your AWS credentials
-aws_access_key_id = AWS_ACCESS_KEY_ID
-aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+# import os
+# import boto3
+# from config.settings.production import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME 
+# # Define your AWS credentials
+# aws_access_key_id = AWS_ACCESS_KEY_ID
+# aws_secret_access_key = AWS_SECRET_ACCESS_KEY
 
-# Create a boto3 client for S3
-s3 = boto3.client('s3',
-                  aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key)
+# # Create a boto3 client for S3
+# s3 = boto3.client('s3',
+#                   aws_access_key_id=aws_access_key_id,
+#                   aws_secret_access_key=aws_secret_access_key)
 
-model_file = "modelreal/pytorch_model.bin"
+# model_file = "modelreal/pytorch_model.bin"
 
-if not os.path.exists(model_file):
-    s3.download_file(AWS_STORAGE_BUCKET_NAME, "pytorch_model.bin", model_file)
+# if not os.path.exists(model_file):
+#     s3.download_file(AWS_STORAGE_BUCKET_NAME, "pytorch_model.bin", model_file)
