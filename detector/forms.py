5421084eb0487c4a4dbef8ff714e86c4a6c0186f
Dilu-Bilu <dilreetraju@gmail.com>
2023-02-14 18:55:28 -0800

    Added front end changes.

--- detector/forms.py
+++ detector/forms.py
@@ -3,7 +3,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field
 
 class TextInputForm(forms.Form):
-    text_input = forms.CharField(label= "", widget=forms.Textarea(attrs={'rows': 12, 'cols': 50, 'class': 'custom-field', 'placeholder': 'Enter your text...', 'style': 'background-color: #FFF8DB; color: black; font-family: Source Code Pro;'} ))
+    text_input = forms.CharField(label= "", widget=forms.Textarea(attrs={'rows': 12, 'cols': 50, 'class': 'custom-field', 'placeholder': 'Enter the text you want to test for AI/Chatbot plagiarism (minimum 20 words).', 'style': 'background-color: #FFF8DB; color: black; font-family: Source Code Pro;'} ))
 
 from .models import Feedback
 
